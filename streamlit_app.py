e228952a3637d57b3ac6660e8a42f3079a26791e
wteoh85 <66007298+wteoh85@users.noreply.github.com>
2023-12-20 10:07:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,10 @@ import pandas
 import requests
 import snowflake.connector
 
-
-streamlit.text("THE FRIUT LOAD LIST CONTAINS:")
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("fruitlist contains:")
+streamlit.text(my_data_row)
+ 
