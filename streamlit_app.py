ecdb7d6ec46ea6b616850044c347d92da6e90614
wteoh85 <66007298+wteoh85@users.noreply.github.com>
2023-12-20 10:06:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,5 @@ import pandas
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 
 streamlit.text("THE FRIUT LOAD LIST CONTAINS:")
-streamlit.text(my_data_row)
